7a15aa843c2e4477bac25a436afb0c77b6b6b66d
Gabriel <gabriel@dekcsign.com>
2019-08-16 21:04:25 -0400

    Add redux configuration

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import store from './store.js';
 import App from './components/App/App.jsx';
 import './Index.scss';
 
 const Index = () => {
-  return <App />;
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 };
 
 ReactDOM.render(<Index />, document.querySelector("#root"));
